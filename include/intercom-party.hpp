036b4f2543d97a169742be6eed4dbc51c4d39383
Wicker25 <wicker25@gmail.com>
2017-04-03 00:51:50 +0100

    Added main header

---
+++ include/intercom-party.hpp
@@ -0,0 +1,17 @@
+/*!
+ * Title ---- intercom-party.hpp
+ * Author --- Giacomo Trudu aka `Wicker25` - wicker25[at]gmail[dot]com
+ *
+ * Copyright (C) 2017 by Giacomo Trudu.
+ * All rights reserved.
+ */
+
+#ifndef __INTERCOM_PARTY_HPP__
+#define __INTERCOM_PARTY_HPP__
+
+#include <intercom-party/version.hpp>
+
+// Define main namespace
+namespace intercom { }
+
+#endif /* __INTERCOM_PARTY_HPP__ */
